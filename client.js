d31f5bcec486b1b7d9999719867a679c826d4c76
Nisarg <nisargvv@gmail.com>
2020-12-02 19:09:00 +0000

    Added message sending to the server

--- client.js
+++ client.js
@@ -8,6 +8,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: NVV");
+  });
+  
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
